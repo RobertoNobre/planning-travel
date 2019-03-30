c5dcb2ca359b63af47fd8e750d29bb673f08d47b
Roberto Nobre <robertonobre90@gmail.com>
2019-03-30 18:27:30 -0300

    node_side - User Signup validat

---
+++ node-api/src/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const UserSchema = new mongoose.Schema({
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+     },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+UserSchema.plugin(mongoosePaginate);
+
+mongoose.model("User", UserSchema);
--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const routes = express.Router();
 
 const TravelController = require("./controllers/TravelController");
+const UserController = require("./controllers/UserController");
 
 routes.get('/travels', TravelController.index);
 routes.get('/travels/:id', TravelController.show);
 routes.post('/travels', TravelController.store);
 routes.put('/travels/:id', TravelController.update);
 routes.delete('/travels/:id', TravelController.destroy);
+routes.post('/auth/signup', UserController.signup);
 
 module.exports = routes;
